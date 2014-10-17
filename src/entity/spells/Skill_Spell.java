31890fa038e458b0e53f9411b6f2d20b6d86b3b7
Joel <posiden104@gmail.com>
2014-10-16 20:01:29 -0400

    SPELL ANGLES!!!
    I updated a LOT for the spells to be able to move on an angle.
    
    The now all spells collide with walls and travel along a smooth path to
    their target

--- src/entity/spells/Skill_Spell.java
+++ src/entity/spells/Skill_Spell.java
@@ -3,45 +3,54 @@ package entity.spells;
 import entity.mob.Mob;
 import graphics.Screen;
 import level.Spawner;
+import main.Game;
 
 public class Skill_Spell extends Spell {
-	
-	//private double spellRange;
 
+	// private double spellRange;
+	private int tanX, tanY;
 
-	public Skill_Spell(int x, int y, int targetX, int targetY) {
+	public Skill_Spell(double x, double y, int targetX, int targetY, int tanX, int tanY) {
 		super(x, y, targetX, targetY);
+		this.tanX = tanX;
+		this.tanY = tanY;
+		angle = angleCalc();
 	}
 
 	public Skill_Spell() {
+	}
+
+	protected double angleCalc() {
+		double dx = tanX - Game.width / 2;
+		double dy = tanY - Game.height / 2;
+		return Math.atan2(dy, dx);
 	}
 
 	public void update() {
 
 		checkMove();
 
-		if (x == targetX) xa = 0;
-		if (y == targetY) ya = 0;
+		if (x == targetX)
+			xa = 0;
+		if (y == targetY)
+			ya = 0;
 
-		if (xa != 0 || ya != 0) {
-			move(xa, ya);
-			x += xa;
-			y += ya;
-		}
+		move(xa, ya);
 
-		if (x == targetX && y == targetY) {
+		if ((x <= targetX + 5 && x >= targetX - 5) && (y <= targetY + 5 && y >= targetY - 5)) {
 			checkHit();
 			this.removed = true;
 		}
 	}
 
 	public void render(Screen screen, int range) {
-		if (!removed) screen.renderItem(x, y, sprite);
-		
-		//drawing the circle
-		int midX = this.x + (sprite.WIDTH / 2);
-		int midY = this.y + (sprite.HEIGHT / 2);
-		//System.out.println("circle range: " + range);
+		if (!removed)
+			screen.renderItem((int) x, (int) y, sprite);
+
+		// drawing the circle
+		int midX = (int) this.x + (sprite.WIDTH / 2);
+		int midY = (int) this.y + (sprite.HEIGHT / 2);
+		// System.out.println("circle range: " + range);
 		for (int y = (midY - range); y <= (midY + range); y++) {
 			for (int x = (midX - range); x <= (midX + range); x++) {
 				if (distance(x, y, midX, midY) == range) {
@@ -56,10 +65,10 @@ public class Skill_Spell extends Spell {
 		Mob selected;
 		for (int i = 0; i < Spawner.mobs.size(); i++) {
 			selected = Spawner.mobs.get(i);
-			if (cornerCheck(x, y, selected, range)) {
+			if (cornerCheck((int) x, (int) y, selected, range)) {
 				selected.changeHealth(damage);
 				selected.hit = true;
-				System.out.println(selected);
+				// System.out.println(selected);
 			}
 		}
 		removed = true;
@@ -76,17 +85,17 @@ public class Skill_Spell extends Spell {
 		return b;
 	}
 
-	protected boolean cornerCheck(int x, int y, Mob selected, int range){
+	protected boolean cornerCheck(int x, int y, Mob selected, int range) {
 		int TL, TR, BL, BR;
 		TL = dist(x, y, selected.xL, selected.yT);
 		TR = dist(x, y, selected.xR, selected.yT);
 		BL = dist(x, y, selected.xL, selected.yB);
 		BR = dist(x, y, selected.xR, selected.yB);
-		if(TL < range || TR < range || BL < range || BR < range)
+		if (TL < range || TR < range || BL < range || BR < range)
 			return true;
 		return false;
 	}
-	
+
 	public int getRange() {
 		return range;
 	}
